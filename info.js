6362369f097b9a7de1305c478a146bd22037965a
star dragon <59759431+stardragoncoder@users.noreply.github.com>
2021-10-16 15:18:57 +0530

    fixed bug

---
+++ info.js
@@ -0,0 +1,24 @@
+const info = document.createElement("div");
+info.classList.add("info");
+let sName = "Shravan.Kumar".toUpperCase().split("");
+let div1 = document.createElement("div");
+let h2 = document.createElement("h2");
+let h4 = document.createElement("h4");
+
+info.appendChild(div1);
+div1.appendChild(h2);
+div1.appendChild(h4);
+let idx = 0;
+let i = setInterval(() => {
+  if (sName[idx] == ".") {
+    h2.innerHTML += "&nbsp;";
+    idx++;
+  } else h2.innerHTML += sName[idx++];
+  if (idx == sName.length) clearInterval(i);
+}, 300);
+
+h2.innerText;
+h4.innerText = "A Frontend Developer based in India";
+setTimeout(() => {
+  document.body.appendChild(info);
+}, 1000);
