8b6742f0613354a8a4c9b111b1ae767326079797
star dragon <59759431+stardragoncoder@users.noreply.github.com>
2021-11-14 16:55:09 +0530

    hover effect

--- info.js
+++ info.js
@@ -2,7 +2,7 @@ const info = document.createElement("div");
 info.classList.add("info");
 let sName = "Shravan.Kumar".toUpperCase().split("");
 let about = "A Frontend Developer from India &";
-let aboutAppend = "🇮🇳";
+let flagAppend = "🇮🇳";
 let div1 = document.createElement("div");
 let h2 = document.createElement("h2");
 let h4 = document.createElement("h4");
@@ -14,13 +14,14 @@ info.appendChild(div1);
 div1.appendChild(h2);
 div1.appendChild(h4);
 let idx = 0;
+let blurStep = 20 / (sName.length - 1);
 let i = setInterval(() => {
   if (sName[idx] == ".") {
     h2.innerHTML += "&nbsp;";
     idx++;
   } else h2.innerHTML += sName[idx++];
 
-  let blurVal = 20 - idx * 2;
+  let blurVal = 20 - blurStep * idx;
   if (blurVal < 0) {
     document.getElementById("solarSystem").style.filter = `none`;
   } else
@@ -48,7 +49,7 @@ h4.innerHTML = about
     } else if (letter === "&") {
       return `<span style="transition-delay:${
         idx * delays.h4
-      }ms">${aboutAppend}</span>`;
+      }ms">${flagAppend}</span>`;
     }
     return `<span style="transition-delay:${
       idx * delays.h4
